3249cebd868d53f288bb9aab5d1737fdc24dd22d
anurag bagri <anurag123bagri@gmail.com>
2025-10-14 16:32:08 +0530

    added better-auth authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
-import { Button } from "@/components/ui/button";
+import { requireAuth } from "@/lib/auth-utils"
+import { caller } from "@/trpc/server";
+import LogoutButton from "./logout";
 
-export default function Home() {
+export default async function Home() {
+  await requireAuth(); // protected server components
+  const data = await caller.getUsers(); 
   return (
-    <div className="text-2xl flex justify-center">
-     <Button className="bg-amber-400 text-amber-800" variant="destructive">
-      click me
-      </Button>
+    <div className="min-h-screen text-2xl flex items-center justify-center">
+       protected server components
+     <div>
+        {JSON.stringify(data)}
+        <LogoutButton/>
+     </div>
     </div>
   )
 }
